427a322184c2c6038783d09cc7d2dac6a8d25b71
Johannes Unterguggenberger <johannes.unterguggenberger@gmail.com>
2021-03-14 18:59:20 +0100

    Update cookie-consent.js

--- assets/js/cookie-consent.js
+++ assets/js/cookie-consent.js
@@ -1,3 +1,4 @@
+window.cookieconsent.initialise({
   "palette": {
     "popup": {
       "background": "#343c66",
@@ -8,5 +9,37 @@
     }
   },
   "theme": "classic",
-  "type": "opt-in"
+  "type": "opt-in",
+  onInitialise: function (status) {
+    var type = this.options.type;
+    var didConsent = this.hasConsented();
+    if (type == 'opt-in' && didConsent) {
+      // enable cookies
+      loadGAonConsent();
+    }
+    if (type == 'opt-out' && !didConsent) {
+      // disable cookies
+    }
+  },
+  onStatusChange: function(status, chosenBefore) {
+    var type = this.options.type;
+    var didConsent = this.hasConsented();
+    if (type == 'opt-in' && didConsent) {
+      // enable cookies
+      loadGAonConsent();
+    }
+    if (type == 'opt-out' && !didConsent) {
+      // disable cookies
+    }
+  },
+  onRevokeChoice: function() {
+    var type = this.options.type;
+    if (type == 'opt-in') {
+      // disable cookies
+    }
+    if (type == 'opt-out') {
+      // enable cookies
+      loadGAonConsent();
+    }
+  }
 });
